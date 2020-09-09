107dcdbdc24730efe9eedb83786b64a8c1cad859
zhaolei <zhaolei@shopex.cn>
2020-09-09 18:07:37 +0800

    update

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/easierway/concurrent_map v0.0.0-20190103024436-7073b0dd7e95
+	github.com/gin-gonic/gin v1.6.3
 	github.com/jinzhu/configor v1.2.0
 )
--- main.go
+++ main.go
@@ -23,10 +23,68 @@ import (
 	"studygo/str"
 	"studygo/structs"
 	syncpool "studygo/sync_pool"
+
+	"github.com/gin-gonic/gin"
 	// "github.com/jinzhu/configor"
 )
 
 func main() {
+	r := gin.Default()
+	// r.GET("/user", func(c *gin.Context) {
+	// 	// name := c.Param("name")
+	// 	a := c.Query("a")
+	// 	b := c.DefaultQuery("b", "123")
+	// 	// cc := c.PostForm("c")
+	// 	// dd := c.DefaultPostForm("d", "dd")
+	// 	// c.String(http.StatusOK, "hello %s %s", a, b)
+	// 	// c.String(http.StatusOK, "hello %s %s", c, d)
+	// 	c.JSON(200, gin.H{
+	// 		"a": a,
+	// 		"b": b,
+	// 		// "c": cc,
+	// 		// "d": dd,
+	// 	})
+
+	// })
+	v1 := r.Group("/v1")
+	{
+		v1.POST("/user", func(c *gin.Context) {
+			a := c.Query("a")
+			b := c.DefaultQuery("b", "123")
+			cc := c.PostForm("c")
+			dd := c.DefaultPostForm("d", "dd")
+			// c.String(http.StatusOK, "hello %s %s", a, b)
+			// c.String(http.StatusOK, "hello %s %s", c, d)
+			c.JSON(200, gin.H{
+				"a": a,
+				"b": b,
+				"c": cc,
+				"d": dd,
+			})
+
+		})
+	}
+
+	v2 := r.Group("/v2")
+	{
+		v2.POST("/user", func(c *gin.Context) {
+			a := c.Query("a")
+			b := c.DefaultQuery("b", "123")
+			cc := c.PostForm("c")
+			dd := c.DefaultPostForm("d", "dd")
+			// c.String(http.StatusOK, "hello %s %s", a, b)
+			// c.String(http.StatusOK, "hello %s %s", c, d)
+			c.JSON(200, gin.H{
+				"a": a,
+				"b": b,
+				"c": cc,
+				"d": dd,
+			})
+
+		})
+	}
+
+	r.Run(":8081")
 	// fmt.Println("out", configor.Config{})
 	// fmt.Println("in", array.Main(123))
 	array.Main()
